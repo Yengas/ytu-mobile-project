b1cc4374e853585a60a6e7c17a96ab93dccc38da
Tolga Ay <tolgaay@live.com>
2016-01-02 17:38:54 +0200

    Add Add Contact funtionality

--- app/src/main/java/phonebookpp/ytu/com/phonebookpp/ContactListActivity.java
+++ app/src/main/java/phonebookpp/ytu/com/phonebookpp/ContactListActivity.java
@@ -1,14 +1,20 @@
 package phonebookpp.ytu.com.phonebookpp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputFilter;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.activeandroid.ActiveAndroid;
 import com.activeandroid.query.Select;
@@ -78,6 +84,66 @@ public class ContactListActivity extends AppCompatActivity implements View.OnCli
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.list_menu_add) {
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+                // Name
+                LinearLayout layout_name = new LinearLayout(this);
+                layout_name.setOrientation(LinearLayout.HORIZONTAL);
+                    TextView label_name = new TextView(this);
+                    label_name.setText("Name: ");
+                layout_name.addView(label_name);
+                    final EditText input_name = new EditText(this);
+                    input_name.setFilters(new InputFilter[]{new InputFilter.LengthFilter(20)});
+                layout_name.addView(input_name);
+            layout.addView(layout_name);
+
+                // Surname
+                LinearLayout layout_surname = new LinearLayout(this);
+                layout_surname.setOrientation(LinearLayout.HORIZONTAL);
+                    TextView label_surname = new TextView(this);
+                    label_surname.setText("Surname: ");
+                layout_surname.addView(label_surname);
+                    final EditText input_surname = new EditText(this);
+                    input_surname.setFilters(new InputFilter[]{new InputFilter.LengthFilter(20)});
+                layout_surname.addView(input_surname);
+            layout.addView(layout_surname);
+            
+                // Email
+                LinearLayout layout_email = new LinearLayout(this);
+                layout_email.setOrientation(LinearLayout.HORIZONTAL);
+                    TextView label_email = new TextView(this);
+                    label_email.setText("Email: ");
+                layout_email.addView(label_email);
+                    final EditText input_email = new EditText(this);
+                    input_email.setFilters(new InputFilter[]{new InputFilter.LengthFilter(20)});
+                layout_email.addView(input_email);
+            layout.addView(layout_email);
+
+            // Setting Dialog
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Add Number");
+            alertDialog.setView(layout);
+            alertDialog.setPositiveButton("ADD",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            String name = input_name.getText().toString();
+                            String surname = input_surname.getText().toString();
+                            String email = input_email.getText().toString();
+
+                            if (name.compareTo("") != 0) {
+                                Contact con = new Contact();
+                                con.name = name;
+                                con.surname = surname;
+                                con.email = email;
+                                con.save();
+
+                                Toast.makeText(getApplicationContext(),
+                                        "New contact \"" + con.name + " " + con.surname + "\" is saved.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+            alertDialog.show(); // Showing Alert Message
             return true;
         }
 
